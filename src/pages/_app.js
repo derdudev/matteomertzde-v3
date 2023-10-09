eb0870006034fada30e3714e869874a741b1d48a
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2023-10-09 21:55:19 +0200

    added page title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import './globals.css';
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps}></Component>;
+    return <>
+        <Head>
+            <title>Matteo Mertz</title>
+        </Head>
+        <Component {...pageProps}></Component>
+    </>
 }
